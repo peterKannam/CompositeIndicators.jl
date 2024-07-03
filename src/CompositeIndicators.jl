338010a149c04c779f1e0958f25b64eb294d6f8a
peterkannam <68120649+peterkannam@users.noreply.github.com>
2024-07-03 10:50:46 -0700

    Update CompositeIndicators.jl

--- src/CompositeIndicators.jl
+++ src/CompositeIndicators.jl
@@ -4,19 +4,22 @@ include("coin_struct.jl")
 export Coin, 
     new_coin, 
     get_meta, 
-    levelnormalizeweights!
+    levelnormalizeweights!,
+    find_parent,
+    find_children,
+    collect_lineages
 
 include("normalize.jl")
 export normalize!,
-norm_competepercentile,
-norm_skipzeropercentile,
-norm_minmax
+    norm_competepercentile,
+    norm_skipzeropercentile,
+    norm_minmax
 
 include("aggregate.jl")
 export aggregate!,
-reaggregate_excludeindicator!,
-ag_geomean,
-ag_mean,
-ag_prod
+    reaggregate_excludeindicator!,
+    ag_geomean,
+    ag_mean,
+    ag_prod
 
 end
