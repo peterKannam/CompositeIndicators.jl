319e624e6c5c6c33c4d4b68da7173ba77c630f44
peterkannam <68120649+peterkannam@users.noreply.github.com>
2024-07-03 11:03:01 -0700

    Update coherence.jl

--- src/coherence.jl
+++ src/coherence.jl
@@ -1,3 +1,5 @@
+using MultivariateStats
+
 function indicatorcorrelation(coin::Coin;
     datakey=:d_original,indicators= names(coin.data[datakey]),resultkey=:cor_current, cor_function = cor,write2coin = false)
 
@@ -11,4 +13,16 @@ function indicatorcorrelation(coin::Coin;
     return o
 end
 
+function indicatorpca(coin::Coin;
+    datakey=:d_original,indicators= names(coin.data[datakey]),resultkey=:pca_model, maxoutdim = 2,write2coin = false)
+
+    d = coin.data[datakey][:,indicators]
+    #generate PCA model. Matrix transpose occurs to reduce indicator dimensions instead of unit dimensions
+    m = fit(PCA,Matrix(d)',maxoutdim = maxoutdim)
+
+    if write2coin
+        ceji.results[resultkey] = m
+        end
+    return m
+end
 
